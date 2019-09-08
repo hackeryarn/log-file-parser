27b05d73cdba45195b20e686ec7af5d46e7135c9
hackeryarn <artemchernyak@gmail.com>
2019-09-08 07:46:29 -0500

    parsing and basic agrigation

--- src/LogFileParser.hs
+++ src/LogFileParser.hs
@@ -1,6 +1,83 @@
 module LogFileParser
        ( someFunc
+       , Log (..)
+       , Activity (..)
+       , Time (..)
+       , timeByActivity
+       , parseLog
+       , parseDay
+       , parseDate
+       , parseComment
+       , parseActivity
        ) where
+
+import Control.Applicative
+import qualified Data.Map.Lazy as M
+import Data.Map.Lazy (Map)
+import Text.Trifecta
+import Data.Char (isSpace)
+import Data.List (dropWhileEnd)
+
+type Date = String
+
+type Hour = Int
+type Minute = Int
+data Time = Time Hour Minute
+  deriving (Eq, Show)
+
+type ActivityDescription = String
+data Activity = Activity Time ActivityDescription
+  deriving (Eq, Show)
+
+newtype Log =
+  Log (Map Date [Activity])
+  deriving (Eq, Show)
+
+
+timeByActivity :: Log -> Map ActivityDescription Time
+timeByActivity (Log days) = foldr (M.unionWith combineDays) mempty activitiesByDay
+  where
+    combineDays (Time h m) (Time h' m') = Time (h + h') (m + m')
+    activitiesByDay = M.foldr go [] days
+    go activities acc = (M.fromList $ zipWith timeSpent activities (tail activities)) : acc
+    timeSpent (Activity (Time h m) description) (Activity (Time h' m') _) =
+      (description, Time (h' - h) (m' - m))
+
+parseLog :: Parser Log
+parseLog = Log <$> days
+  where days = M.fromList <$> manyTill (skipMany eol *> parseDay) eof
+
+parseDay :: Parser (Date, [Activity])
+parseDay = (,) <$> parseDate <*> some parseActivity
+
+parseDate :: Parser Date
+parseDate = char '#' *> spaces *> go <* (try eol <|> (whiteSpace *> eol))
+  where go = do
+          year <- count 4 digit
+          _ <- char '-'
+          month <- count 2 digit
+          _ <- char '-'
+          day <- count 2 digit
+          return $ year ++ "-" ++ month ++ "-" ++ day
+
+parseComment :: Parser ()
+parseComment =
+  () <$ (try (char '-') *> char '-' *> manyTill anyChar (try eol))
+
+parseActivity :: Parser Activity
+parseActivity = Activity <$> parseTime <*> parseActivityDescription
+
+parseActivityDescription :: Parser ActivityDescription
+parseActivityDescription =
+  dropWhileEnd isSpace <$> manyTill anyChar (try eol)
+
+parseTime :: Parser Time
+parseTime = Time <$> (read <$> parseHour) <*> (read <$> parseMinute)
+  where parseHour = count 2 digit <* char ':'
+        parseMinute = count 2 digit <* spaces
 
 someFunc :: IO ()
 someFunc = putStrLn ("someFunc" :: String)
+
+eol :: Parser ()
+eol = eof <|> parseComment <|> (() <$ newline)
--- test/Spec.hs
+++ test/Spec.hs
@@ -1,4 +1,147 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE QuasiQuotes #-}
+
 module Main (main) where
 
+import qualified Data.Map as M
+import Test.Hspec
+import Text.Trifecta
+import Text.RawString.QQ
+import LogFileParser
+
+testLog :: String
+testLog = [r|
+-- wheee a comment
+
+# 2025-02-05
+08:00 Breakfast
+09:00 Sanitizing moisture collector
+11:00 Exercising in high-grav gym
+12:00 Lunch
+13:00 Programming
+17:00 Commuting home in rover
+17:30 R&R
+19:00 Dinner
+21:00 Shower
+21:15 Read
+22:00 Sleep
+
+# 2025-02-07 -- dates not nececessarily sequential
+08:00 Breakfast -- should I try skippin bfast?
+09:00 Bumped head, passed out
+13:36 Wake up, headache
+13:37 Go to medbay
+13:40 Patch self up
+13:45 Commute home for rest
+14:15 Read
+21:00 Dinner
+21:15 Read
+22:00 Sleep
+|]
+
+parsedTestLog :: Log
+parsedTestLog =
+  Log $
+  M.fromList
+    [ ( "2025-02-05"
+      , [ Activity (Time 8 0) "Breakfast"
+        , Activity (Time 9 0) "Sanitizing moisture collector"
+        , Activity (Time 11 0) "Exercising in high-grav gym"
+        , Activity (Time 12 0) "Lunch"
+        , Activity (Time 13 0) "Programming"
+        , Activity (Time 17 0) "Commuting home in rover"
+        , Activity (Time 17 30) "R&R"
+        , Activity (Time 19 0) "Dinner"
+        , Activity (Time 21 0) "Shower"
+        , Activity (Time 21 15) "Read"
+        , Activity (Time 22 0) "Sleep"
+        ])
+    , ( "2025-02-07"
+      , [ Activity (Time 8 0) "Breakfast"
+        , Activity (Time 9 0) "Bumped head, passed out"
+        , Activity (Time 13 36) "Wake up, headache"
+        , Activity (Time 13 37) "Go to medbay"
+        , Activity (Time 13 40) "Patch self up"
+        , Activity (Time 13 45) "Commute home for rest"
+        , Activity (Time 14 15) "Read"
+        , Activity (Time 21 0) "Dinner"
+        , Activity (Time 21 15) "Read"
+        , Activity (Time 22 0) "Sleep"
+        ])
+    ]
+
+singleDay :: String
+singleDay = [r|# 2025-02-05
+08:00 Breakfast
+09:00 Sanitizing moisture collector
+11:00 Exercising in high-grav gym
+12:00 Lunch
+13:00 Programming
+17:00 Commuting home in rover
+17:30 R&R
+19:00 Dinner
+21:00 Shower
+21:15 Read
+22:00 Sleep
+|]
+
+parsedSingleDay :: (String, [Activity])
+parsedSingleDay =
+  ( "2025-02-05"
+  , [ Activity (Time 8 0) "Breakfast"
+    , Activity (Time 9 0) "Sanitizing moisture collector"
+    , Activity (Time 11 0) "Exercising in high-grav gym"
+    , Activity (Time 12 0) "Lunch"
+    , Activity (Time 13 0) "Programming"
+    , Activity (Time 17 0) "Commuting home in rover"
+    , Activity (Time 17 30) "R&R"
+    , Activity (Time 19 0) "Dinner"
+    , Activity (Time 21 0) "Shower"
+    , Activity (Time 21 15) "Read"
+    , Activity (Time 22 0) "Sleep"
+    ])
+
+maybeSuccess :: Result a -> Maybe a
+maybeSuccess (Success a) = Just a
+maybeSuccess _ = Nothing
+
 main :: IO ()
-main = putStrLn ("Test suite not yet implemented" :: String)
+main = hspec $ do
+  let ps = parseString
+  describe "comment parser" $ do
+    let pspc = ps parseComment mempty
+    it "skips lone comment" $ do
+      let result = pspc "-- dates not nececessarily sequential"
+      maybeSuccess result `shouldBe` Just ()
+  describe "date parser" $ do
+    let pspd = ps parseDate mempty
+    it "parses simple date" $ do
+      let result = pspd "# 2025-02-05"
+      maybeSuccess result `shouldBe` Just "2025-02-05"
+    it "skips comments after date" $ do
+      let result = pspd "# 2025-02-07 -- dates not nececessarily sequential"
+      maybeSuccess result `shouldBe` Just "2025-02-07"
+  describe "activity parser" $ do
+    let pspa = ps parseActivity mempty
+    it "parses an activity" $ do
+      let result = pspa "08:00 Breakfast"
+      maybeSuccess result `shouldBe` Just (Activity (Time 8 0) "Breakfast")
+    it "parses an activity ignoring comment" $ do
+      let result = pspa "08:00 Breakfast -- good"
+      maybeSuccess result `shouldBe` Just (Activity (Time 8 0) "Breakfast")
+    it "parses an activity with dash in the middle" $ do
+      let result = pspa "08:00 Breakfast - good"
+      maybeSuccess result `shouldBe` Just (Activity (Time 8 0) "Breakfast - good")
+    it "parses an activity with a dash and a comment" $ do
+      let result = pspa "08:00 Breakfast - good -- best so far"
+      maybeSuccess result `shouldBe` Just (Activity (Time 8 0) "Breakfast - good")
+  describe "day parser" $ do
+    let pspd = ps parseDay mempty
+    it "parses a day" $ do
+      let result = pspd singleDay
+      maybeSuccess result `shouldBe` Just parsedSingleDay
+  describe "log parser" $ do
+    let pspl = ps parseLog mempty
+    it "parses a multi day log" $ do
+      let result = pspl testLog
+      maybeSuccess result `shouldBe` Just parsedTestLog
